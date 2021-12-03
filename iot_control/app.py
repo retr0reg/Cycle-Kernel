a91d467fbe43df61c3aa0fd6e7294fd5a4715a0e
DDizzzy79 <Patrick_0429@163.com>
2021-12-03 18:09:48 +0800

    V2

--- iot_control/app.py
+++ iot_control/app.py
@@ -2,6 +2,13 @@ from flask import Flask,render_template
 import RPi.GPIO as GPIO
 import time
 import lcd1602 as lcd
+import time
+from luma.led_matrix.device import *
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.legacy import text
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial, cascaded=1 or 1, block_orientation=0,rotate=0 or 0, blocks_arranged_in_reverse_order=False)
 id = 1
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(20,GPIO.OUT)
@@ -51,7 +58,36 @@ def clear():
     id = 0
     lcd.clear_lcd()
     return render_template("main.html")
-    
+
+@app.route("/left")
+def left():
+    for x in range(10):
+        #print("drawing")
+        for x in range(5):
+            with canvas(device) as draw:
+                text(draw, (0, 0), chr(27), fill="white")
+                time.sleep(0.01)
+    return render_template("main.html")
+            
+@app.route("/right")
+def right():
+    for x in range(10):
+        #print("drawing")
+        for x in range(4):
+            with canvas(device) as draw:
+                text(draw, (0, 0), chr(26), fill="white")
+                time.sleep(0.01)
+    return render_template("main.html")
+
+@app.route("/line")
+def line():
+    for x in range(10):
+        #print("drawing")
+        for x in range(4):
+            with canvas(device) as draw:
+                text(draw, (0, 0), chr(24), fill="white")
+                time.sleep(0.01)
+    return render_template("main.html")
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
